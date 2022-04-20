39245ca2e0445f68d82335e840b022b443d29492
javierhilarioxmarts <javier.hilario.xmarts@gmail.com>
2022-04-20 17:02:33 -0500

    [REF] Migrate to version 15.0

--- addendas_soriana/__manifest__.py
+++ addendas_soriana/__manifest__.py
@@ -1,35 +1,18 @@
 # -*- coding: utf-8 -*-
 {
     'name': "addendas_soriana",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
+    'summary': """Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    'description': """Long description of module's purpose""",
+    'author': "Xmarts",
+    'contributors': "victoralonso@xmarts.com, javier.hilario@xmarts.com",
+    'website': "http://www.xmarts.com",
     'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base','l10n_mx_edi'],
-
-    # always loaded
+    'version': '15.0.1.0.0',
+    'depends': ['base', 'l10n_mx_edi', 'fields_functions_jarochito'],
     'data': [
         # 'security/ir.model.access.csv',
         'views/addenda_sorianaFN.xml',
         'views/addenda_sorianaFE.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
