e47abaaccd0f4e62bba8a49170689719ff519f8e
javierhilarioxmarts <javier.hilario.xmarts@gmail.com>
2022-11-10 12:36:44 -0600

    [UPDATE] Migrate to version 14.0 - addenda_sorianaFN

---
+++ addendas_soriana/models/inherit_account_move.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    def _get_values_addenda(self):
+        print('---------------------------_get_values_addenda-----------------------------')
+        for rec in self:
+            for l in rec.edi_document_ids:
+                cfdi_3_3_edi = self.env.ref('l10n_mx_edi.edi_cfdi_3_3')
+                if l.edi_format_id == cfdi_3_3_edi:
+                    invoice = l.move_id
+                    xml = l.edi_format_id._l10n_mx_edi_get_invoice_cfdi_values(invoice)
+                    print('xml', xml)
+                    return l.edi_format_id._l10n_mx_edi_get_invoice_cfdi_values(invoice)
